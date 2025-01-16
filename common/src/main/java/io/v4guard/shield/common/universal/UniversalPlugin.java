06d3e25bd388d38fc3fcd9e51418204ca819603a
Alejandro G <contacto@zhacked.me>
2025-01-16 04:15:34 +0100

    feat: create plugin messaging logic with custom channel

--- common/src/main/java/io/v4guard/shield/common/universal/UniversalPlugin.java
+++ common/src/main/java/io/v4guard/shield/common/universal/UniversalPlugin.java
@@ -3,11 +3,13 @@ package io.v4guard.shield.common.universal;
 import io.v4guard.shield.api.service.ConnectedCounterService;
 import io.v4guard.shield.common.ShieldCommon;
 import io.v4guard.shield.common.hook.AuthenticationHook;
+import io.v4guard.shield.common.messenger.PluginMessenger;
 
 public interface UniversalPlugin {
 
     ShieldCommon getCommon();
     ConnectedCounterService getConnectedPlayers();
     AuthenticationHook getActiveAuthenticationHook();
+    PluginMessenger getMessenger();
     boolean isPluginEnabled(String pluginName);
 }
