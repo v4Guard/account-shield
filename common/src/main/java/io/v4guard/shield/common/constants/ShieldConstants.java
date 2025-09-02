eb3a5dfe88de14c89ce7ce17e179d990636fb506
Alejandro G <contacto@zhacked.me>
2025-09-02 12:04:15 +0200

    fix!: reduce size of accshield channel

--- common/src/main/java/io/v4guard/shield/common/constants/ShieldConstants.java
+++ common/src/main/java/io/v4guard/shield/common/constants/ShieldConstants.java
@@ -2,7 +2,7 @@ package io.v4guard.shield.common.constants;
 
 public class ShieldConstants {
 
-    public static final String MODERN_PLUGIN_MESSAGE_CHANNEL = "v4guard:accountshield";
+    public static final String MODERN_PLUGIN_MESSAGE_CHANNEL = "v4guard:accshield";
     public static final String LEGACY_PLUGIN_MESSAGE_CHANNEL = "AccountShield";
 
 }
